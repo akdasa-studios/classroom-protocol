463bd5d85a7580b18334c95029d5249f2dce7181
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-12-28 20:26:38 +0000

    chore: Refactor AsyncTask class to remove unused code

--- core/AsyncTask.ts
+++ core/AsyncTask.ts
@@ -9,63 +9,32 @@ export interface AsyncTaskState {
   progress?: number
 }
 
-export type AsyncTaskStateChanged = (a: AsyncTaskState) => void
-
-export interface AsyncTaskOptions {
-  stateChanged?: AsyncTaskStateChanged
+export interface IAsyncTask<
+  TRequest extends Request,
+  TResponse extends Response
+> {
+  execute(request: TRequest): Promise<TResponse>
 }
 
 export abstract class AsyncTask<
   TRequest extends Request,
   TResponse extends Response
 > {
-  private _stateChanged?: AsyncTaskStateChanged;
-
-  constructor(
-    options?: AsyncTaskOptions
-  ) {
-    this._stateChanged = options?.stateChanged
-  }
 
   protected abstract onWork(
     request: TRequest
   ): Promise<TResponse>;
 
-  public subscribeStateChanged(callback: AsyncTaskStateChanged) {
-    this._stateChanged = callback
-  }
-
   async execute(
     request: TRequest
   ): Promise<TResponse> {
     try {
-      // Notify that task is stared
-      if (this._stateChanged) {
-        this._stateChanged({ isInProgress: true })
-      }
-
-      const result = await this.onWork(request)
-
-      // Notify
-      if (this._stateChanged) {
-        this._stateChanged({ isInProgress: false })
-      }
-
-      return result
+      return await this.onWork(request)
     } catch (error) {
       const errorDescription = {
         code: error instanceof KnownError ? error.code : KnownErrors.UnknownError,
         message: error as string
       }
-
-      // Notify that task is stared
-      if (this._stateChanged) {
-        this._stateChanged({
-          isInProgress: false,
-          error: errorDescription
-        })
-      }
-
       return {
         error: errorDescription
       } as TResponse
