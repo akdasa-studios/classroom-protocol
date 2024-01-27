109229fbe59b246d01297281c278677a672f24f7
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-01-27 16:38:34 +0000

    Add email and status fields to UserScheme

--- lib/user/UserScheme.ts
+++ lib/user/UserScheme.ts
@@ -1,7 +1,14 @@
+import type { UserStatus } from '@classroom/core/aggregates'
+
 export interface UserScheme {
   id: string
   name: string
+  email: string,
   roles: UserRoleScheme[]
+  status: UserStatus
+  title?: string
+  department?: string
+  avatarUrl?: string
 }
 
 export interface UserRoleScheme {
