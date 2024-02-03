410d9d5f483a1c29cb9b429e399d862ce8937640
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-02-03 12:05:16 +0000

    Add optional and required fields for user creation and define updatable fields for user update

--- lib/user/UserScheme.ts
+++ lib/user/UserScheme.ts
@@ -1,5 +1,8 @@
 // import type { UserStatus } from '@classroom/core/aggregates'
 
+type OptionalFields<TScheme, K extends keyof TScheme> = Partial<Pick<TScheme, K>>
+type RequiredFields<TScheme, K extends keyof TScheme> = Pick<TScheme, K>
+
 export interface UserScheme {
   id: string
   name: string
@@ -18,9 +21,33 @@ export interface GetUsersResponse {
   data: UserScheme[]
 }
 
+/* -------------------------------------------------------------------------- */
+/*                                   Create                                   */
+/* -------------------------------------------------------------------------- */
 
-export interface UpdateUserRequest
-  extends Partial<Pick<UserScheme, 'name' | 'email' | 'roles' | 'status' | 'title' | 'department'>> {
+
+export interface CreateUserRequest extends
+  RequiredFields<UserScheme, 'name' | 'email' | 'roles'>,
+  OptionalFields<UserScheme, 'title' | 'department'>
+{
+}
+
+export interface CreateUserResponse {
+  success: boolean
+  message?: string
+}
+
+
+
+/* -------------------------------------------------------------------------- */
+/*                                   Update                                   */
+/* -------------------------------------------------------------------------- */
+
+type UserUpdatableFields = 'name' | 'email' | 'roles' | 'status' | 'title' | 'department'
+
+export interface UpdateUserRequest extends
+  OptionalFields<UserScheme, UserUpdatableFields>
+{
 }
 
 export interface UpdateUserResponse {
